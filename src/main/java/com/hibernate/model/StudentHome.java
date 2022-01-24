f2c3946fe488b1cf56f6c28b6946ae8be1520780
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-01-24 21:05:31 +0530

    first-commit

---
+++ src/main/java/com/hibernate/model/StudentHome.java
@@ -0,0 +1,116 @@
+package com.hibernate.model;
+// Generated 22 Jan 2022, 15:14:03 by Hibernate Tools 5.5.7.Final
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Student.
+ * @see com.hibernate.model.Student
+ * @author Hibernate Tools
+ */
+public class StudentHome {
+
+	private static final Logger logger = Logger.getLogger(StudentHome.class.getName());
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext().lookup("SessionFactory");
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Student transientInstance) {
+		logger.log(Level.INFO, "persisting Student instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			logger.log(Level.INFO, "persist successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Student instance) {
+		logger.log(Level.INFO, "attaching dirty Student instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			logger.log(Level.INFO, "attach successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Student instance) {
+		logger.log(Level.INFO, "attaching clean Student instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			logger.log(Level.INFO, "attach successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Student persistentInstance) {
+		logger.log(Level.INFO, "deleting Student instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			logger.log(Level.INFO, "delete successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "delete failed", re);
+			throw re;
+		}
+	}
+
+	public Student merge(Student detachedInstance) {
+		logger.log(Level.INFO, "merging Student instance");
+		try {
+			Student result = (Student) sessionFactory.getCurrentSession().merge(detachedInstance);
+			logger.log(Level.INFO, "merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "merge failed", re);
+			throw re;
+		}
+	}
+
+	public Student findById(int id) {
+		logger.log(Level.INFO, "getting Student instance with id: " + id);
+		try {
+			Student instance = (Student) sessionFactory.getCurrentSession().get("com.hibernate.model.Student", id);
+			if (instance == null) {
+				logger.log(Level.INFO, "get successful, no instance found");
+			} else {
+				logger.log(Level.INFO, "get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Student instance) {
+		logger.log(Level.INFO, "finding Student instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("com.hibernate.model.Student")
+					.add(Example.create(instance)).list();
+			logger.log(Level.INFO, "find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "find by example failed", re);
+			throw re;
+		}
+	}
+}
